de23be4f4882a3bd282365c18ac6aeef3d2f0ea8
Endrina Toledo <71093001+endrina-toledo@users.noreply.github.com>
2022-03-27 18:22:11 -0400

    Modificaciones de rutas de usuarios

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,10 +2,10 @@ const userController = require('../controllers/userControllers.js')
 
 const router = require('express').Router()
 
-router.post('/addUser', userController.addUser)
-router.get('/getAllUsers', userController.getAllUsers)
-router.get('/getOneUserById/:usuId', userController.getOneUserById)
-router.put('/updateUser/:usuId', userController.updateUser)
-router.delete('/deleteUser/:usuId', userController.deleteUser)
+router.post('/', userController.addUser)
+router.get('/', userController.getAllUsers)
+router.get('/:usuId', userController.getOneUserById)
+router.put('/:usuId', userController.updateUser)
+router.delete('/:usuId', userController.deleteUser)
 
 module.exports = router
