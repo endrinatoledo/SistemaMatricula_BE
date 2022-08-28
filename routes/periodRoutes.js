cc575f59ad1877cb4b8fef502af21c0e0206bbce
Endrina Toledo <endrx12@gmail.com>
2022-08-28 17:30:31 -0400

    se agrega la ruta de buscar periodo por id

--- routes/periodRoutes.js
+++ routes/periodRoutes.js
@@ -4,6 +4,7 @@ const router = require('express').Router()
 
 router.post  ('/', periodController.addPeriod)
 router.get   ('/', periodController.getAllPeriods)
+router.get   ('/:perId', periodController.getOnePeriodById)
 router.get   ('/onePeriod/active', periodController.getOneActivePeriod)
 router.put   ('/:perId', periodController.updatePeriod)
 router.delete('/:perId', periodController.deletePeriod)
